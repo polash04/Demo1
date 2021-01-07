92f97ad0fe8c077b59cc8f940ae904569b60d86c
Polash04 <polash04@yahoo.com>
2021-01-07 10:33:39 +0100

    yes

---
+++ git/src/Main.java
@@ -0,0 +1,8 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Lite kod här
+    }
+}
---
+++ git/src/MyClass.java
@@ -0,0 +1,8 @@
+
+
+public class MyClass {
+
+    void functionality (){
+        //massor kod här
+    }
+}
